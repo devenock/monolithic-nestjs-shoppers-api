c8969822a08155c3ad0ded91428ec80d53417f36
devenock <enockomondi305@gmail.com>
2025-10-01 18:44:18 +0300

    project setup and initial modules

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { UsersModule } from './modules/users/users.module';
+import { ProductsModule } from './modules/products/products.module';
+import { AuthModule } from './modules/auth/auth.module';
+
+@Module({
+  imports: [UsersModule, ProductsModule, AuthModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
