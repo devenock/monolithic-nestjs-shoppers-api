d3e33e91291b7f17859f64f1b27cfa719fde9b09
devenock <enockomondi305@gmail.com>
2025-10-02 10:49:23 +0300

    added redis caching layer

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './modules/users/users.module';
 import { ProductsModule } from './modules/products/products.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { User } from './modules/users/user.entity';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
   imports: [
@@ -25,6 +27,23 @@ import { User } from './modules/users/user.entity';
       // SET IT AS GLOBAL
       isGlobal: true,
     }),
+    // CACHING
+    // we can use different stores like redis, memory, etc
+    // here we are using memory store
+    // CacheModule.register({
+    //   isGlobal: true, // Make the cache module global
+    //   store: ['memory'], // Use in-memory caching
+    //   ttl: 5, // Default time-to-live (seconds)
+    //   max: 10, // Maximum number of items in cache
+    // }),
+    // using REDIS as cache store
+    CacheModule.register({
+      isGlobal: true, 
+      store: redisStore,
+      host: 'redis',
+      port: 6379,
+    }), 
+    // MODULES
     UsersModule,
     ProductsModule,
     AuthModule,
