c1091590c1794c1d654155445b6664ddaf2b6c79
devenock <enockomondi305@gmail.com>
2025-10-13 12:11:13 +0300

    adding swagger constants

---
+++ src/config/constants/swagger.constants.ts
@@ -0,0 +1,4 @@
+export const SWAGGER_TITLE = 'Shoppers API';
+export const SWAGGER_API_VERSION = '1.0';
+export const SWAGGER_API_DESCRIPTION = 'Monolithic Shoppers API Documentation';
+export const SWAGGER_API_ROOT = 'api/v1/docs';
